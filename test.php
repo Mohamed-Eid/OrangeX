0808f09faf78cab156436bd13f4c59879179cc30
Mohamed Eid <medoeid50@gmail.com>
2019-04-10 09:55:08 +0200

    test commit

--- test.php
+++ test.php
@@ -4,40 +4,18 @@ require_once 'includes/core/DB.php';
 
 
 $db = new DB();
+$table = 'departments';
 
 
-echo $db->Select_Count('migrations');
-
-
-
-function Insert($table,$data)
+$data = $db->GetAll($table);
+for($i=0;$i<count($data);$i++)
 {
-
-    // setup some variables for fields and values
-  $fields  = '';
-    $values = '';
-    // populate them
-    foreach ($data as $f => $v)
-    {
-        $fields  .= "`$f`,";
-        $values .= ( is_numeric( $v ) && ( intval( $v ) == $v ) ) ? $v."," : "'$v',";
-    }
-
-    // remove our trailing ,
-  $fields = substr($fields, 0, -1);
-  // remove our trailing ,
-  $values = substr($values, 0, -1);
-
-  echo "fields : ".$fields;
-  echo "values : ".$values;
-    // $querystring = "INSERT INTO `{$table}` ({$fields}) VALUES({$values})";
-    // //echo $querystring;
-    // //Check If Row Inserted
-    // if($this->Execute($querystring))
-    //     return TRUE;
-    //
-    // return FALSE;
+  foreach ($data[$i] as $key => $value) {
+    echo $key." : ".$value."<br>";
+  }
 }
 
-Insert("users",['user'=>'ahmed','id'=>2]);
+
+echo"<pre>";
+print_r($data);
 ?>
